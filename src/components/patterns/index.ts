730c3c997d0f95af652e8c26c99c5112e8843dea
AlexaVJ <alexandra19vela@gmail.com>
2026-08-03 21:53:05 -0500

    chore: remove unused UserStats and StatsGrid components

--- src/components/patterns/index.ts
+++ src/components/patterns/index.ts
@@ -5,11 +5,9 @@ export { EntityDetailPage } from './EntityDetailPage'
 
 // Component Patterns
 export { DataTable } from './DataTable'
-export { StatsGrid } from './StatsGrid'
 export { SearchAndFilter } from './SearchAndFilter'
 export { EmptyState } from './EmptyState'
 
 // Types
 export type { Column } from './DataTable'
-export type { StatItem } from './StatsGrid'
 export type { FilterConfig } from './SearchAndFilter'
